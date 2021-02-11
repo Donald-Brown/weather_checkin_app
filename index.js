ecc80e7cf863c92a65fe033f5599025993cf2034
donald-brown <donbrown4652@gmail.com>
2021-02-11 13:29:17 -0600

    moves port to process.env

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ require('dotenv').config();
 // build server
 const express = require("express");
 const myServer = express();
-const port = 3000;
+const port = process.env.PORT || 3000;
 
 //build database
 const database = new Datastore("database.db");
